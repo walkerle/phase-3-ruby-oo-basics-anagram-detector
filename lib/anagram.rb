57a5677e7e2b9b9dd47e977ef5a310af0de14fd9
Walker Le <walker.le88@gmail.com>
2023-01-31 16:18:33 -0700

    commit tree dbe4e34ab8ccc002fd653a616f7b1b7f6e968765

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+  def initialize word
+    @word = word
+  end
+
+  def split_word
+    @word.chars.sort
+  end
+
+  def match match_array
+    match_array.filter { |element| element.chars.sort == split_word }
+  end
+
+end
